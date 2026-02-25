baf9d652fde92c95be6d772a7347a800a7c78556
John Madden <jmadden173@pm.me>
2026-02-24 18:27:17 -0800

    Migrated to ents python package

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 from dirtviz_slack_bot.cli import entry
 
+
 def lambda_handler(event, context):
     entry()
     return {"status": "ok"}
-
--- src/dirtviz_slack_bot/cli.py
+++ src/dirtviz_slack_bot/cli.py
@@ -1,19 +1,19 @@
-
 from datetime import datetime, timedelta
 from typing import Generator
 
-import pandas as pd
-import requests
 from slack_sdk import WebClient
 import argparse
+from ents.dirtviz import BackendClient
 
-from yaml import load, dump
+from yaml import load
+
 try:
-    from yaml import CLoader as Loader, CDumper as Dumper
+    from yaml import CLoader as Loader
 except ImportError:
-    from yaml import Loader, Dumper
+    from yaml import Loader
 
 import logging
+
 logger = logging.getLogger(__name__)
 
 SLACK_TOKEN = ""
@@ -21,8 +21,9 @@ CLIENT_ID = "#soil-power-students"
 
 
 class Group:
-    def __init__(self, name : str, start: int, end: int, include:
-                 list[int], exclude: list[int]):
+    def __init__(
+        self, name: str, start: int, end: int, include: list[int], exclude: list[int]
+    ):
         self._name = name
         self.start = start
         self.end = end
@@ -33,7 +34,7 @@ class Group:
         return self._name
 
     def cells(self) -> list[int]:
-        cells = list(range(self.start, self.end+1))
+        cells = list(range(self.start, self.end + 1))
         cells += self.include
         cells = [c for c in cells if c not in self.exclude]
 
@@ -42,7 +43,6 @@ class Group:
 
 class Config:
     """Configuration class for the Dirtviz API client."""
-
 
     def __init__(self, data: dict):
         self.data = data
@@ -61,186 +61,7 @@ class Config:
 
     def groups(self) -> Generator:
         for g in self.data["groups"]:
-            yield Group(g["name"], g["start"], g["end"], g["include"],
-                        g["exclude"])
-
-
-class Cell:
-    """Class representing a cell in the Dirtviz API."""
-
-    def __init__(self, data: dict):
-        """Initialize the Cell object from a cell ID.
-
-        Args:
-            data: json data from the Dirtviz API containing cell information.
-        """
-
-        self.id = data["id"]
-        self.name = data["name"]
-        self.location = data["location"]
-        self.latitude = data["latitude"]
-        self.longitude = data["longitude"]
-
-    def __repr__(self):
-        return f"Cell(id={self.cell_id}, name={self.name})"
-
-
-class BackendClient:
-    """Client for interacting with the Dirtviz API."""
-
-    DEFAULT_BASE_URL = "https://dirtviz.jlab.ucsc.edu/api/"
-
-    def __init__(self, base_url: str = DEFAULT_BASE_URL):
-        """Initialize the BackendClient.
-
-        Sets the base URL for the API. Defaults to the Dirtviz API.
-        """
-
-        self.base_url = base_url
-
-    def get(self, endpoint: str, params: dict = None) -> dict:
-        """Get request to the API.
-
-        Args:
-            endpoint: The API endpoint to request.
-            params: Optional parameters for the request.
-
-        Returns:
-            A dictionary containing the response data.
-        """
-
-        url = f"{self.base_url}{endpoint}"
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-
-        return response.json()
-
-    def power_data(self, cell: Cell, start: datetime, end: datetime) -> pd.DataFrame:
-        """Gets power data for a specific cell.
-
-        Args:
-            cell: The Cell object for which to get power data.
-            start: The start date of the data.
-            end: The end date of the data.
-
-        Returns:
-            A pandas DataFrame containing the power data.
-        """
-
-        start_str = self.format_time(start)
-        end_str = self.format_time(end)
-
-        endpoint = f"/power/{cell.id}"
-        params = {
-            "startTime": start_str,
-            "endTime": end_str,
-        }
-
-        data = self.get(endpoint, params=params)
-
-        data_df = pd.DataFrame(data)
-        data_df["timestamp"] = pd.to_datetime(data_df["timestamp"])
-
-        return data_df
-
-    def teros_data(self, cell: Cell, start: datetime, end: datetime) -> pd.DataFrame:
-        """Gets teros data for a specific cell.
-
-        Args:
-            cell: The Cell object for which to get teros data.
-            start: The start date of the data.
-            end: The end date of the data.
-
-        Returns:
-            A pandas DataFrame containing the teros data.
-        """
-
-        start_str = self.format_time(start)
-        end_str = self.format_time(end)
-
-        endpoint = f"/teros/{cell.id}"
-        params = {
-            "startTime": start_str,
-            "endTime": end_str,
-        }
-
-        data = self.get(endpoint, params=params)
-
-        data_df = pd.DataFrame(data)
-        data_df["timestamp"] = pd.to_datetime(data_df["timestamp"])
-
-        return data_df
-
-    @staticmethod
-    def format_time(dt: datetime) -> str:
-        """Formats a datetime object to the API's expected string format.
-
-        Args:
-            dt: The datetime object to format.
-
-        Returns:
-            A string representing the formatted datetime.
-        """
-
-        timestamp_format = "%a, %d %b %Y %H:%M:%S GMT"
-        dt_str = dt.strftime(timestamp_format)
-        return dt_str
-
-    def cell_from_id(self, cell_id: int) -> Cell | None:
-        """Get a Cell object from its ID.
-
-        Args:
-            cell_id: The ID of the cell.
-
-        Returns:
-            A Cell object. None if the cell does not exist.
-        """
-
-        cell_list = self.cells()
-
-        for cell in cell_list:
-            if cell.id == cell_id:
-                return cell
-
-        return None
-
-    def cell_from_name(self, name: str) -> Cell | None:
-        """Get a Cell object from its name.
-
-        Args:
-            name: The name of the cell.
-
-        Returns:
-            A Cell object. None if the cell does not exist.
-        """
-
-        cell_list = self.cells()
-
-        for cell in cell_list:
-            if cell.name == name:
-                return cell
-
-        return None
-
-    def cells(self) -> list[Cell]:
-        """Gets a list of all cells from the API.
-
-        Returns:
-            A list of Cell objects.
-        """
-
-        cell_list = []
-
-        endpoint = "/cell/id"
-        cell_data_list = self.get(endpoint)
-
-        for c in cell_data_list:
-            cell = Cell(c)
-            cell_list.append(cell)
-
-        return cell_list
-
-
+            yield Group(g["name"], g["start"], g["end"], g["include"], g["exclude"])
 
 
 def post_hello_world(client: WebClient):
@@ -263,7 +84,7 @@ def post_cell_data(client: WebClient, name: str, channel: str, cells: list[int])
     backend = BackendClient()
 
     end = datetime.utcnow()
-    start = end - timedelta(days=1) 
+    start = end - timedelta(days=1)
 
     # top level message
     msg = f"*{name}*\n\n"
@@ -278,19 +99,38 @@ def post_cell_data(client: WebClient, name: str, channel: str, cells: list[int])
             logger.warning("Cell ID %d not found", cid)
             continue
 
-
         power_df = backend.power_data(cell, start, end)
         teros_df = backend.teros_data(cell, start, end)
+
+        new_voltage_df = backend.sensor_data_simple(
+            cell,
+            "POWER_VOLTAGE",
+            "Voltage",
+            start,
+            end
+        )
+
+        new_teros_vwc_df = backend.sensor_data_simple(
+            cell,
+            "POWER_VOLTAGE",
+            "Voltage",
+            start,
+            end
+        )
+
+        
+
 
         if power_df.empty or teros_df.empty:
             msg += f"- {cell.name}\n"
             logger.warning("No data for cell %s (%d)", cell.name, cell.id)
 
-        voltage = power_df['v'].iloc[-1] if not power_df.empty else 'No Data'
-        current = power_df['i'].iloc[-1] if not power_df.empty else 'No Data'
-        vwc = teros_df['vwc'].iloc[-1] if not teros_df.empty else 'No Data'
-        temp = teros_df['temp'].iloc[-1] if not teros_df.empty else 'No Data'
-        ec = teros_df['ec'].iloc[-1] if not teros_df.empty else 'No Data'
+        voltage = power_df["v"].iloc[-1] if not power_df.empty else "No Data"
+        current = power_df["i"].iloc[-1] if not power_df.empty else "No Data"
+
+        vwc = teros_df["vwc"].iloc[-1] if not teros_df.empty else "No Data"
+        temp = teros_df["temp"].iloc[-1] if not teros_df.empty else "No Data"
+        ec = teros_df["ec"].iloc[-1] if not teros_df.empty else "No Data"
 
         thread += f"*{cell.name}*:\n"
         thread += f"\tv:    {voltage} mV\n"
@@ -300,18 +140,19 @@ def post_cell_data(client: WebClient, name: str, channel: str, cells: list[int])
         thread += f"\tec:   {ec} dS/m\n"
         thread += "\n"
 
-
-
     resp = client.chat_postMessage(channel=channel, text=msg)
     thread_ts = resp["ts"]
 
     client.chat_postMessage(channel=channel, text=thread, thread_ts=thread_ts)
 
+
 def entry():
     """Entrypoint for the slack bot"""
 
     parser = argparse.ArgumentParser(description="Dirtviz Slack Bot")
-    parser.add_argument("--verbose", "-v", action="store_true", help="Enable verbose logging")
+    parser.add_argument(
+        "--verbose", "-v", action="store_true", help="Enable verbose logging"
+    )
     parser.add_argument("config", type=str, help="Path to configuration file")
     args = parser.parse_args()
 
@@ -331,10 +172,8 @@ def entry():
         cells = g.cells()
         logger.info(f"Posting data for {name}: {cells}")
 
-
         # post actual data
         post_cell_data(client, name, config.channel(), cells)
-
 
 
 if __name__ == "__main__":
